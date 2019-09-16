a54d0e2f8979395febaffa31c45dce7f490e640f
fanpengfei <552999964@qq.com>
2019-09-16 20:54:44 +0800

    fw

--- class_opt_overload.cpp
+++ class_opt_overload.cpp
@@ -10,18 +10,35 @@ class Box {
 
 	public:
 		Box(int l=10);
+		
 		int getLength();
 		void setLength(int x);
-		
-		/* 一元操作重载 */ 
+
+		/* 一元云算法： 赋值 */
+		Box operator=(Box &b) {
+			this->length = b.length;
+		}
+
+		/* 二元操作重载 */
 		Box operator+(const Box &b);
 		bool operator<(const Box &b);
-		
-		/* 自增运算符 */ 
+
+		/* 自增运算符 */
 		Box operator++ ();
 		Box operator++ (int);
-		
-		/* 友缘 输入/输出运算符 */ 
+
+		/* 函数调用运算符()重载 */
+		Box operator() (int x, int y, int z) {
+			Box b(0);
+			
+			b.length +=x;
+			b.length *=y;
+			b.length -=z;
+			
+			return b;
+		}
+		 
+		/* 友员 输入/输出运算符 */
 		friend ostream &operator << (ostream &output, const Box &b) {
 			output << "Length:" << b.length << endl;
 			return output;
@@ -93,14 +110,21 @@ int main() {
 	cout << "a < b ? " << (c < b) << endl;
 
 	cout << a << endl;
-	
+
 	cout << b << endl;
-	
+
 	cout << "before++" << a++ << endl;
 	cout << "after" << a << endl;
-	
+
 	cout << "++before" << ++a << endl;
 	cout << "after" << a << endl;
+
+	Box d = b;
+
+	cout << "d: " << d << endl;
 	
+	Box e = b(10, 2, 5); /* (0 + 10)*2 - 5 = 15 */
+	cout << "e: " << e << endl;
+
 	return 0;
 }
