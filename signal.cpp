647e0e7ce3215374b38decb35686c66bcdd6ddf3
fanpengfei1 <fanpengfei@jd.com>
2019-09-30 17:54:53 +0800

    commit signal

---
+++ signal.cpp
@@ -0,0 +1,37 @@
+/****************************************************************************
+@file:signal.cpp 
+@author:ebayboy@163.com 
+@date:2019-09-30 17:45 
+@version 1.0  
+@description: cpp file 
+@Copyright (c)  all right reserved 
+**************************************************************************/
+
+#include <iostream>
+#include <csignal>
+#include <unistd.h>
+
+using namespace std;
+
+
+void signalHandler(int signum)
+{
+	cout << "Interrupt signal (" << signum << endl;
+
+	exit(signum);
+}
+
+int main(int argc, char **argv)
+{
+	cout<<"start ok!"<<endl;
+
+	signal(SIGINT, signalHandler);
+
+	while(1) {
+		cout << "going to sleep ..." << endl;
+		sleep(1);
+	}
+
+	return 0;
+}
+
