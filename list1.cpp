c5877da70be44912e165c6b71a7189e71e9e065f
fanpengfei1 <fanpengfei@jd.com>
2019-10-14 14:09:18 +0800

    commit list1

--- list1.cpp
+++ list1.cpp
@@ -6,15 +6,20 @@ using namespace std;
 void list_int()
 {
 	//用list容器处理整型数据
-	
-	//用LISTINT创建一个名为listOne的list对象
+	//用创建一个名为listOne的list对象
+	list<int> listOne;
 	
 	//声明i为迭代器
+	list<int>::iterator i;
 	
 	//从前面向listOne容器中添加数据
-	
+	listOne.push_front(1);
+	listOne.push_front(2);
+
 	//从后面向listOne容器中添加数据
-	
+	listOne.push_back(3);
+	listOne.push_back(4);
+
 	//从前向后显示listOne中的数据
 	
 	//从后向后显示listOne中的数据
