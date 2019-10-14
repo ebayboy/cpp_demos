5fb3615e404d0c7f4ac8bae7feb4a273bcf99e7b
fanpengfei1 <fanpengfei@jd.com>
2019-10-14 13:52:03 +0800

    commit list1

--- list1.cpp
+++ list1.cpp
@@ -6,12 +6,19 @@ using namespace std;
 void list_int()
 {
 	//用list容器处理整型数据
+	
 	//用LISTINT创建一个名为listOne的list对象
+	
 	//声明i为迭代器
+	
 	//从前面向listOne容器中添加数据
+	
 	//从后面向listOne容器中添加数据
+	
 	//从前向后显示listOne中的数据
+	
 	//从后向后显示listOne中的数据
+	
 	//使用STL的accumulate(累加list所有元素)算法
 }
 
@@ -20,11 +27,17 @@ void list_char()
 	//--------------------------
 	//用list容器处理字符型数据
 	//--------------------------
+	
 	//用LISTCHAR创建一个名为listOne的list对象
+	
 	//声明i为迭代器
+	
 	//从前面向listTwo容器中添加数据
+	
 	//从后面向listTwo容器中添加数据
+	
 	//从前向后显示listTwo中的数据
+	
 	//使用STL的max_element算法求listTwo中的最大元素并显示
 }
 
