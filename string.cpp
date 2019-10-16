16154c1e926094ad45a92fcb289e947d4bb04f4f
fanpengfei1 <fanpengfei@jd.com>
2019-10-16 15:38:24 +0800

    commit string

--- string.cpp
+++ string.cpp
@@ -19,6 +19,21 @@ int main()
 	
 	len = str3.size();
 	cout << "len:" << len << endl;
-	
+
+	string str4("str4");
+	cout << "str4: " << str4 << endl;
+	cout << "str4: size: " << str4.size() << endl;
+	cout << "str4: length: " << str4.length() << endl;
+	cout << "str4[2]:" << str4[2] << endl;
+
+	cout << "ret: " << str4.compare("str4") << endl;
+	cout << "ret: " << str4.compare("str45") << endl;
+
+	// 比较str4的前三位和后一个字符串
+	cout << "ret: " << str4.compare(0, 3, "str") << endl;
+
+	// 比较str4和后一个字符串的前缀
+	cout << "ret: " << str4.compare(0, str4.size(), "str45", 0, str4.size()) << endl;
+
 	return 0;
 }
