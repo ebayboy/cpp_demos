e7f1bf8f522ec04f9bfbc74ddc3355cc7dbeac6d
fanpengfei1 <fanpengfei@jd.com>
2019-10-17 16:43:26 +0800

    fw

--- copy.cpp
+++ copy.cpp
@@ -1,10 +1,10 @@
 /****************************************************************************
-@file:copy.cpp 
-@author:ebayboy@163.com 
-@date:2019-10-17 16:19 
-@version: 1.0  
-@description: cpp file 
-@Copyright (c)  all right reserved 
+@file:copy.cpp
+@author:ebayboy@163.com
+@date:2019-10-17 16:19
+@version: 1.0
+@description: cpp file
+@Copyright (c)  all right reserved
 **************************************************************************/
 
 #include <iostream>
@@ -15,25 +15,38 @@
 
 using namespace std;
 
-#define DEBUG 
+#define DEBUG
+
+
+void show(vector<int> v)
+{
+    for (size_t i = 0; i < v.size(); i++)
+    {
+        cout << v[i] << " ";
+    }
+    cout << endl << endl;
+}
 
 int main(int argc, char **argv)
 {
-	vector<int> v1 = { 1, 2, 3};
-	vector<int> v2 = { 4, 5};
+    vector<int> v1 = {1, 2, 3};
+    vector<int> v2 = {4, 5};
 
-	std::copy(v1.begin(), v1.end(), v2.begin());
+    //保存v2大小
+    size_t putpos = v2.size();
 
-	cout << "v1.size(): " << v1.size() << endl;
-	cout << "v2.size(): " << v2.size() << endl;
+    cout << "before resive v2 size: " << v2.size() <<endl;
 
-	cout << "show v2:" << endl;
-	for (size_t i = 0; i < v2.size(); i++) {
-		cout << v2[i] << " ";
-	}
+    //申请可以容纳v1.size()的空间
+    v2.resize(v2.size() + v1.size());
+    cout << "after resive: " << v2.size() <<endl;
+    show(v2);
 
-	cout << endl;
+    //将v1元素拷贝到v2的末尾
+    std::copy(v1.begin(), v1.end(), v2.begin() + putpos);
+    cout << "after copy show v2:" << endl;
+    show(v2);
 
-	return 0;
+    return 0;
 }
 
