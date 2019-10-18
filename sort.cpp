9eaf7259e89de1754745140582bbba9457336123
fanpengfei1 <fanpengfei@jd.com>
2019-10-18 18:48:55 +0800

    commit sort

--- sort.cpp
+++ sort.cpp
@@ -24,11 +24,11 @@ int main(int argc, char **argv)
 	cout<<"hello world!"<<endl;
 
 	vector<int> v = {4, 5, 1, 2, 3};
-	SHOW_VEC(v, "v: ");
+	SHOW_VEC(v);
 
 	//sort by default
 	std::sort(std::begin(v), std::end(v));
-	SHOW_VEC(v, "v");
+	SHOW_VEC(v);
 
 	//sort by self function
 	vector<int> v1 = {4, 5, 1, 2, 3};
@@ -53,14 +53,22 @@ int main(int argc, char **argv)
 
 #endif
 
-	SHOW_VEC(v1, "v1");
+	SHOW_VEC(v1);
 
 	//partial sort
 	vector<int> v2 = { 4,5, 1, 3, 2};
-	SHOW_VEC(v2, "v2");
+	SHOW_VEC(v2);
 	std::partial_sort(std::begin(v2), std::begin(v2) + 2, std::end(v2));
-	SHOW_VEC(v2, "v2");
-	
+	SHOW_VEC(v2);
+
+	//nth_element 高效版排序算法
+	//4 5 1 3 2 6  -> 2 1 3 4 5 6 
+	vector<int> v3 = { 4,5, 1, 3, 2, 6};
+	SHOW_VEC(v3);
+	cout << "std::begin(v3) + 3: " << *(v3.begin() + 3) << endl;
+	std::nth_element(std::begin(v3), std::begin(v3) + 3, std::end(v3));
+	SHOW_VEC(v3);
+
 	return 0;
 }
 
