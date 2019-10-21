01ab75e555a5b11fb172dd17de416c65e6421dd9
fanpengfei1 <fanpengfei@jd.com>
2019-10-21 10:55:42 +0800

    commit equal_range

--- equal_range.cpp
+++ equal_range.cpp
@@ -22,15 +22,18 @@ using namespace std;
 int main(int argc, char **argv)
 {
 	vector<int> v = { 1, 3, 5, 5, 5, 7};
-
+	std::sort(v.begin(), v.end());
 	SHOW_VEC(v);
 
 	std::pair<std::vector<int>::iterator,std::vector<int>::iterator> bounds;
 
+	//C++ STL equal_range 函数说明
+	//equal_range 返回范围[first,last)内等于指定值val的子范围的迭代器。
+	//注意的是使用这个函数的前提是范围[first,last)内的元素是有序的。
+	
 	bounds = std::equal_range(std::begin(v), std::end(v), 5);
-	std::cout << "bounds at positions " << (bounds.first - v.begin());
-	//cout << "first: " << bounds->first - v.begin() << endl;
-	cout << "second: " << bounds->second - v.begin() << endl;
+	cout << "bounds at positions " << (bounds.first - v.begin());
+	cout << " and " << (bounds.second - v.begin()) << endl;
 
 	return 0;
 }
