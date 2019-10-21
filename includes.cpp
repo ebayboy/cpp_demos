ff12d4f02fa36e17aed72a08735f8fc5ccbf0d8e
fanpengfei1 <fanpengfei@jd.com>
2019-10-21 11:45:22 +0800

    commit includes

---
+++ includes.cpp
@@ -0,0 +1,30 @@
+/****************************************************************************
+@file:includes.cpp 
+@author:ebayboy@163.com 
+@date:2019-10-21 11:25 
+@version: 1.0  
+@description: cpp file 
+@Copyright (c)  all right reserved 
+**************************************************************************/
+
+#include <iostream>
+#include <string>
+#include <numeric>
+#include <vector>
+#include <algorithm>
+
+using namespace std;
+
+#define DEBUG 
+
+int main(int argc, char **argv)
+{
+	vector<int> v1 = { 1,2,3,4,5 }; 
+	vector<int> v2 = { 2,3,4 }; 
+
+	auto ret = std::includes(v1.begin(), v1.end(), v2.begin(), v2.end());
+	cout << ret << endl;
+
+	return 0;
+}
+
