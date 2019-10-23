2b111eb4279d9a0d41fe736eddaa061aa50fdc3c
fanpengfei1 <fanpengfei@jd.com>
2019-10-23 16:35:52 +0800

    commit math

--- math.cpp
+++ math.cpp
@@ -47,6 +47,18 @@ int main(int argc, char **argv)
 
 	test_tan(45);
 
+	printf("sqrt:%.2f\n", sqrt(5));
+
+	printf("ceil(PI):%.2f\n", ceil(3.14));
+	printf("floor(PI):%.2f\n", floor(3.14));
+
+	printf("e:2.71828\n");
+	printf("exp(x):%.2f\n", exp(3));
+	printf("log(x): %.2f\n", log(8)); 
+	printf("log10: %.2f\n", log10(100));
+	printf("pow(a, b): %.2f\n", pow(2, 4));
+	printf("fmod(a, b): %.2f\n", fmod(3.14, 3.01));
+
 	return 0;
 }
 
