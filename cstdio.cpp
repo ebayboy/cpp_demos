7c891fda1896546d4ddef5638efc2a76f6053e09
fanpengfei1 <fanpengfei@jd.com>
2019-10-23 14:48:06 +0800

    commit cstdio

--- cstdio.cpp
+++ cstdio.cpp
@@ -13,7 +13,21 @@
 
 using namespace std;
 
+//qsort
+
 #define DEBUG 
+
+int compar(const void *va, const void *vb)
+{
+	int *a = (int *)va;
+	int *b = (int *)vb;
+
+	if ((*a) > (*b)) {
+		return 1;
+	}
+
+	return 0;
+}
 
 int main(int argc, char **argv)
 {
@@ -32,6 +46,14 @@ int main(int argc, char **argv)
 
 	char d = 'd';
 	printf("toupper(d)=%c tolower(d):%c)\n", toupper(d), tolower(d));
+
+
+	int arr[] = { 3, 5, 2 , 4 , 1};
+	qsort(arr, sizeof(arr)/sizeof(int), sizeof(int), compar);
+	for (size_t i = 0; i < sizeof(arr)/sizeof(int); i ++) {
+		printf("%d ", arr[i]);
+	}
+	printf("\n");
 
 	exit(0);
 
