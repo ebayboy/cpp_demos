bf85854b6ccfef6a2c488006a5f3279bdcb1a74b
fanpengfei1 <fanpengfei@jd.com>
2019-10-23 14:04:48 +0800

    commit iostream

--- iostream.cpp
+++ iostream.cpp
@@ -2,18 +2,23 @@
 
 #include <iostream>
 #include <fstream>
+#include <iomanip>
 
 using namespace std;
 
 int main()
 {
-	ofstream out("./o.txt");
-	
-	char buff[BUFSIZ] = {0};
-	
-	gets(buff);
-	
-	out << buff;
-	
+	int age;
+	cin >> age;
+	cout << age <<endl;
+
+	string name;
+	cin >> name;
+	cout << name << endl;
+
+	cout.width(2);
+	cout.fill('0');
+	cout << age << endl;;
+
 	return 0;
 }
