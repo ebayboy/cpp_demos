787eaefcd8e53f9aa1844e91595833279d6b3862
ebayboy <ebayboy@163.com>
2019-11-19 23:31:31 +0800

    update for build with gcc 4.8

--- source/ormpp/build/CMakeFiles/ormpp.dir/DependInfo.cmake
+++ source/ormpp/build/CMakeFiles/ormpp.dir/DependInfo.cmake
@@ -1,26 +1,26 @@
 # The set of languages for which implicit dependencies are needed:
-set(CMAKE_DEPENDS_LANGUAGES
+SET(CMAKE_DEPENDS_LANGUAGES
   "CXX"
   )
 # The set of files for implicit dependencies of each language:
-set(CMAKE_DEPENDS_CHECK_CXX
+SET(CMAKE_DEPENDS_CHECK_CXX
   "/root/cpp_demos/source/ormpp/main.cpp" "/root/cpp_demos/source/ormpp/build/CMakeFiles/ormpp.dir/main.cpp.o"
   )
-set(CMAKE_CXX_COMPILER_ID "GNU")
+SET(CMAKE_CXX_COMPILER_ID "GNU")
 
 # Preprocessor definitions for this target.
-set(CMAKE_TARGET_DEFINITIONS_CXX
+SET(CMAKE_TARGET_DEFINITIONS
   "ORMPP_ENABLE_MYSQL"
   )
 
+# Targets to which this target links.
+SET(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
 # The include file search paths:
-set(CMAKE_CXX_TARGET_INCLUDE_PATH
+SET(CMAKE_C_TARGET_INCLUDE_PATH
   "../iguana"
   )
-
-# Targets to which this target links.
-set(CMAKE_TARGET_LINKED_INFO_FILES
-  )
-
-# Fortran module output directory.
-set(CMAKE_Fortran_TARGET_MODULE_DIR "")
+SET(CMAKE_CXX_TARGET_INCLUDE_PATH ${CMAKE_C_TARGET_INCLUDE_PATH})
+SET(CMAKE_Fortran_TARGET_INCLUDE_PATH ${CMAKE_C_TARGET_INCLUDE_PATH})
+SET(CMAKE_ASM_TARGET_INCLUDE_PATH ${CMAKE_C_TARGET_INCLUDE_PATH})
--- source/ormpp/build/CMakeFiles/ormpp.dir/cmake_clean.cmake
+++ source/ormpp/build/CMakeFiles/ormpp.dir/cmake_clean.cmake
@@ -1,10 +1,10 @@
-file(REMOVE_RECURSE
+FILE(REMOVE_RECURSE
   "CMakeFiles/ormpp.dir/main.cpp.o"
   "libormpp.pdb"
   "libormpp.a"
 )
 
 # Per-language clean rules from dependency scanning.
-foreach(lang CXX)
-  include(CMakeFiles/ormpp.dir/cmake_clean_${lang}.cmake OPTIONAL)
-endforeach()
+FOREACH(lang CXX)
+  INCLUDE(CMakeFiles/ormpp.dir/cmake_clean_${lang}.cmake OPTIONAL)
+ENDFOREACH(lang)
