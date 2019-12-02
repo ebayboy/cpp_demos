974f3db0ea41d3b8949ba08ff1dbef71fa59bfc6
ebayboy <ebayboy@163.com>
2019-12-02 18:55:57 +0800

    fw

---
+++ lib_so_a/clean.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+# ***************************************************************************
+# * 
+# * @file:clean.sh 
+# * @author:ebayboy@163.com 
+# * @date:2019-12-02 18:55 
+# * @version 1.0  
+# * @description: Shell script 
+# * @Copyright (c)  all right reserved 
+#* 
+#**************************************************************************/ 
+
+rm -rf *.o.*
+rm -rf *.so *.a
+
+exit 0
+
