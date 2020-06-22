05ed62d8aa8e15d9e63c203d3d8e75cc80dbb260
fanpf <ebayboy@aliyun.com>
2020-06-22 19:45:52 +0800

    fw

--- class_this.cpp
+++ class_this.cpp
@@ -1,7 +1,10 @@
 /* class this pointer */
 
-#include <iostream>
+//在 C++ 中，每一个对象都能通过 this 指针来访问自己的地址。
+//this 指针是所有成员函数的隐含参数。因此，在成员函数内部，它可以用来指向调用对象。
+//友元函数没有 this 指针，因为友元不是类的成员。只有成员函数才有 this 指针。
 
+#include <iostream>
 
 using namespace std;
 
