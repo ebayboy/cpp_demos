79aba6caaecb6a97a4944f8ea0eb6f10314be2b3
fanpengfei1 <fanpengfei1@jd.com>
2020-07-08 10:09:19 +0800

    fw

---
+++ .vscode/test.cpp
@@ -0,0 +1,19 @@
+/*
+test cpp
+*/
+#include <iostream>
+#include <string>
+#include <vector>
+
+using namespace std;
+
+int main(int args, char **argv)
+{
+    
+    cout << "hello world" << endl;
+
+    return 0;
+}
+
+
+
