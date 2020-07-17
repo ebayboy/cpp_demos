12ddc1bc8e4c86db2775e5b926d68b0d34bbcd2c
fanpengfei1 <fanpengfei1@jd.com>
2020-07-17 18:30:05 +0800

    fw

--- src/lsaxTest/test_risk.sh
+++ src/lsaxTest/test_risk.sh
@@ -9,6 +9,4 @@ curl -H "Host: risk01.00.com" -v "http://127.1.1.1/1.html?x_usec=fdasfeqwf3DACDA
 curl -H "Host: risk01.00.com" -v "http://127.1.1.1/1.html?x_usec=fdasfeqwf3DACDAEDQDVA23c%2fkeOVAKRoTpJ5TIi4SYD4SpFyNqEOWHN0NsEqDCSsECK7EpSJVZSJVZAJZZWBVYNfDeNyNrGZAHN0SYFcRIFyNq5ZAHN0SYFcRHcoYAWINpufRpRyNqzZAHN0SYJfSIJfRYVyNqzZVeN0RfNdSIVdSIRdlV%3d%3d" --user-agent "/html"
 curl -H "Host: risk01.00.com" -v "http://127.1.1.1/1.html?x_usec=fdasfeqwf3DACDAEDQDVA23c%2fkeOVAKRoTpJ5TIi4SYD4SpFyNqEOWHN0NsEqDCSsECK7EpSJVZSJVZAJZZWBVYNfDeNyNrGZAHN0SYFcRIFyNq5ZAHN0SYFcRHcoYAWINpufRpRyNqzZAHN0SYJfSIJfRYVyNqzZVeN0RfNdSIVdSIRdlV%3d%3d" --user-agent "/html"
 curl -H "Host: risk01.00.com" -v "http://127.1.1.1/1.html?x_usec=fdasfeqwf3DACDAEDQDVA23c%2fkeOVAKRoTpJ5TIi4SYD4SpFyNqEOWHN0NsEqDCSsECK7EpSJVZSJVZAJZZWBVYNfDeNyNrGZAHN0SYFcRIFyNq5ZAHN0SYFcRHcoYAWINpufRpRyNqzZAHN0SYJfSIJfRYVyNqzZVeN0RfNdSIVdSIRdlV%3d%3d" --user-agent "/html"
-
-sleep 3
 done
