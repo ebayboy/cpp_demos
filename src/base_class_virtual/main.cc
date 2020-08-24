951d5ce33ab679c61d60c0f825ea6578e883b97f
ebayboy <ebayboy@163.com>
2020-08-24 12:00:03 +0800

    fw

--- src/base_class_virtual/main.cc
+++ src/base_class_virtual/main.cc
@@ -3,6 +3,12 @@
 #include "class.hpp"
 
 
+//ok
+//Base Constructor
+//Inherit Constructor!
+//Base::print()
+//Inherit Destructor!
+//Base Destructor
 void test_inherit_to_base()
 {
 	Base* pbase = new Inherit();
@@ -12,6 +18,12 @@ void test_inherit_to_base()
 	delete pbase; //ok, delete inherit -> delete base
 }
 
+//ok
+//Base Constructor
+//Inherit Constructor!
+//Inherit::show()
+//Inherit Destructor!
+//Base Destructor
 void test_base_to_inherit()
 {
 	//Base *pbase = new Base(); //Error: will generate memory leak 
@@ -23,9 +35,48 @@ void test_base_to_inherit()
 	delete pInherit;
 }
 
+//error: mem leak
+//Base Constructor
+//Inherit::show()
+void test_base_to_inherit2()
+{
+	Base *pbase = new Base(); //Error: will generate memory leak 
+
+	Inherit *pInherit = dynamic_cast<Inherit*>(pbase); //mem leak
+	if (pInherit == NULL) {
+		cout << "pInherit is NULL!" << endl;
+	}
+
+	Inherit *pInherit_s = static_cast<Inherit*>(pbase); //mem leak
+	if (pInherit_s == NULL) {
+		cout << "pInherit static is NULL!" << endl;
+	} else {
+		cout << "pInherit static is not NULL! not safe" << endl;
+	}
+	
+	pInherit->show();
+
+	delete pInherit;
+}
+
+//mem ok 
+//Base Constructor
+//Inherit::show()
+//Base Destructor
+void test_base_to_inherit3()
+{
+	Base *pbase = new Base(); //Error: will generate memory leak 
+
+	Inherit *pInherit = (Inherit *)pbase;
+	pInherit->show();
+
+	delete pInherit;
+}
+
 int main()
 {
-	test_base_to_inherit();
+	test_base_to_inherit2();
+//	test_inherit_to_base();
 
 	return 0;
 }
