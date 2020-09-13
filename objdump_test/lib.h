aceda0cd76f5cfd84a0447866d196a1cc1817fdc
ebayboy <ebayboy@163.com>
2020-09-13 20:12:22 +0800

    fw

---
+++ objdump_test/lib.h
@@ -0,0 +1,12 @@
+
+#ifndef __LIB_H__
+#define __LIB_H__
+#include <vector>
+
+void print_vec(const std::vector<int>& vec);
+void print_int(int i);
+void print_str(char* str);
+void print_core();
+
+#endif    
+
---
+++ objdump_test/shared/vec.cpp
@@ -0,0 +1,36 @@
+
+#include <iostream>
+#include <vector>
+
+#include "lib.h"
+
+int func_in_main(int a)
+{
+  std::cout << "a:" << a << std::endl;
+
+  return a + 1;
+}
+
+int main()
+{
+  std::vector<int> vec;
+  vec.push_back(1);
+  vec.push_back(2);
+  vec.push_back(3);
+
+  print_vec(vec);
+  print_int(vec.size());
+
+  //test core
+  print_core();
+
+  print_str((char *)"hello world!");
+
+  int f_in = func_in_main(5);
+  std::cout << "f_in:" << f_in << std::endl;
+
+  return 0;
+}
+
+//编译：
+////g++ -o vec vec.cpp
